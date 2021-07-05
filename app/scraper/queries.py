ad5e908716c585761ff4da9d56d736f28ad08cf4
Utkarsh-Kushagra <utkarsh@vengage.ai>
2021-07-05 10:46:48 +0530

    logging cleaned up

--- app/scraper/queries.py
+++ app/scraper/queries.py
@@ -9,12 +9,14 @@ sys.path.append(os.path.realpath(os.path.relpath("../..")))
 logger = logging.getLogger(__name__)
 
 def find_medicine(search_str:str):
+    logger.info(f'Searching {search_str} on MIMS.')
     brand_name,generic_name,isExact = scraper.scraper_fine(search_str)
     medicine = Medicine(brand_name=brand_name,generic_name=generic_name,isExact=isExact,search_name=search_str)
     if medicine.generic_name: medicine.substitutes = get_list_of_substitutes(medicine.generic_name)
     return medicine
 
 def get_list_of_substitutes(generic_name:str):
+    logger.info(f'Finding substitutes for {generic_name}')
     url = os.getenv('DAILY_MED') + generic_name
     response = json.loads(requests.request("GET", url,timeout=5).text)
     data = response['data']
